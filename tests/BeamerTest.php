305bc8d39e6a26632ea2f685e842b01524b34387
Warrick Bayman <me@warrickbayman.com>
2021-06-02 17:15:05 +0200

    wip

---
+++ tests/BeamerTest.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TPG\Tests;
+
+use TPG\Beamer\Facades\Beamer;
+use TPG\Beamer\Streamer;
+
+class BeamerTest extends TestCase
+{
+    /**
+     * @test
+     **/
+    public function it_can_load_a_filesystem_disk(): void
+    {
+        $stream = Beamer::make('test.mp4');
+
+        self::assertInstanceOf(Streamer::class, $stream);
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TPG\Tests;
+
+use Orchestra\Testbench\TestCase as Orchestra;
+use TPG\Beamer\BeamerServiceProvider;
+use TPG\Beamer\Facades\Beamer;
+
+class TestCase extends Orchestra
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->setupStorage();
+    }
+
+    protected function setupStorage(): void
+    {
+        config([
+            'filesystems.disks.beamer' => [
+                'driver' => 'local',
+                'root' => __DIR__,
+            ],
+            'beamer.disk' => 'beamer',
+        ]);
+    }
+
+    protected function getPackageProviders($app): array
+    {
+        return [
+            BeamerServiceProvider::class,
+        ];
+    }
+
+    protected function getPackageAliases($app): array
+    {
+        return [
+            'Beamer' => Beamer::class,
+        ];
+    }
+
+
+}
